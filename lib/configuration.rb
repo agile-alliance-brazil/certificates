ba04aa9e344a0d5c71d620d2180b725ccf34456c
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-15 10:17:58 -0900

    Small refactoring in the configuration

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -3,11 +3,11 @@ require_relative './delivery.rb'
 require_relative './configuration/configuration_error.rb'
 
 class Configuration
-  attr_reader :filename_prefix, :email_sender, :csv_filepath,
+  attr_reader :filename_prefix, :delivery, :email_sender, :csv_filepath,
     :svg_filepath, :body_template_path, :inkscape_path, :cache_folder_path
   def initialize(options)
     @filename_prefix = options[:filename_prefix]
-    @deliveries = Delivery.configure_deliveries(options[:deliveries])
+    @delivery = Delivery.configure_deliveries(options[:deliveries]).first
     @email_sender = options[:deliveries][:sender]
     @csv_filepath = File.expand_path('data.csv', options[:data_folder])
     @svg_filepath = File.expand_path('model.svg', options[:data_folder])
@@ -15,15 +15,16 @@ class Configuration
     @inkscape_path = options[:inkscape_path]
     @cache_folder_path = options[:cache_folder_path]
 
+    raise_error_if_incomplete(options)
+  end
+  private
+  def raise_error_if_incomplete(options)
     if options[:data_folder].nil?
       raise ConfigurationError.new(options[:help])
     elsif @email_sender.nil?
       raise ConfigurationError.new("Missing SENDER information. Please define an environment variable with key name 'SENDER' or add that entry to your .env file.")
-    elsif !@deliveries.first.complete?
-      raise ConfigurationError.new(@deliveries.first.error_messages)
+    elsif !@delivery.complete?
+      raise ConfigurationError.new(@delivery.error_messages)
     end
-  end
-  def delivery
-    @deliveries.first
   end
 end
