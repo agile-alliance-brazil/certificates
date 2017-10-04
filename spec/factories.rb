fe0234ab2b246eed6a12e379e09e42b39671a5e1
katee <hello@kate.io>
2017-10-04 19:53:59 -0400

    Remove unused factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,18 +3,4 @@ FactoryGirl.define do
   factory :attendee do
     initialize_with { new(name: 'John Doe', email: 'john.doe@email.com') }
   end
-
-  factory :certificate_sender do
-    configs(
-      'user' => 'no-reply',
-      'domain' => 'domain.com',
-      'smtp_server' => 'smtp.otherdomain.com',
-      'smtp_port' => '865',
-      'password' => 'fake'
-    )
-    subject 'Fake subject'
-    content_type 'application/pdf'
-
-    initialize_with { new(configs, subject, content_type) }
-  end
 end
