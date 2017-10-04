075f8960526c4e16c9d5c8313ad1777e84b4c597
katee <hello@kate.io>
2017-10-04 18:39:43 -0400

    Replace lib/certicates.rb with lib/certificator.rb

---
+++ lib/certificator.rb
@@ -0,0 +1,8 @@
+# encoding:UTF-8
+require 'certificator/version'
+require 'configuration/certificate_option_parser'
+require 'configuration'
+require 'certificate_generator'
+require 'workers/bulk_sender_worker'
+require 'models/csv_parser'
+require 'models/attendee'
