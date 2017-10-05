ced60e74809cf60c7b5d5fc7e3ce1be2a213d93b
katee <hello@kate.io>
2017-10-05 15:56:03 -0400

    Enable next failure in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,8 @@ require 'certificator/decorators/identity'
 Dir[File.expand_path('support/*.rb', File.dirname(__FILE__))].each do |file|
   require file
 end
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+end
