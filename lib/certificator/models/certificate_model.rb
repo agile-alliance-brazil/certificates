b28e3c591a7fa7705cef539ea5f53c00fb7b260e
Hugo Corbucci <hugo.corbucci@gmail.com>
2018-11-18 18:08:22 -0500

    Fixing generating invalid svg for text fields with weird chars

--- lib/certificator/models/certificate_model.rb
+++ lib/certificator/models/certificate_model.rb
@@ -9,7 +9,13 @@ module Certificator
 
     def svg_for(attendee)
       attendee.attributes.inject(@svg_content) do |result, (attribute, value)|
-        result.gsub(/&lt;#{attribute}&gt;/, value.to_s)
+        result.gsub(
+          /&lt;#{attribute}&gt;/,
+          value.to_s
+            .gsub(/&(?!amp;)/, '&amp;')
+            .gsub(/</, '&lt;')
+            .gsub(/>/, '&gt;')
+        )
       end
     end
   end
--- spec/models/certificate_model_spec.rb
+++ spec/models/certificate_model_spec.rb
@@ -23,4 +23,14 @@ describe Certificator::CertificateModel do
   it 'should replace svg tag <email> with attendee email' do
     expect(model.svg_for(attendee)).to match(/fake@domain.com/i)
   end
+
+  context 'with weird characters' do
+    let(:attendee) do
+      Certificator::Attendee.new(name: 'Name & <others>', email: 'fake@d.com')
+    end
+
+    it 'should replace svg tag <name> with attendee name' do
+      expect(model.svg_for(attendee)).to match(/Name &amp; &lt;others&gt;/i)
+    end
+  end
 end
